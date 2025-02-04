d7378283b3ac126f50d1938669a9bb0293362fa9
Zheero0 <reeco.liburd@outlook.com>
2025-02-04 20:10:27 +0000

    hero

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -32,7 +32,7 @@ export const Hero = () => {
         <div className="mt-6 flex justify-center gap-4">
           <Link
             href={"/post-job"}
-            className="bg-blue-800 hover:bg-blue-700 text-white font-bold py-3 px-5 rounded-full mr-4 transition-all duration-200 transform hover:scale-105"
+            className="bg-blue-800 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full mr-4 transition-all duration-200 transform hover:scale-105"
           >
             Post a Job
           </Link>
